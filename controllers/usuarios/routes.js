10baeb1da090789bcf3a230cb42834c43b74e213
Luciano Saavedra <lsaaved5@externos.bancoestado.cl>
2024-07-18 19:35:25 -0400

    Sacamos a la monica

--- controllers/usuarios/routes.js
+++ controllers/usuarios/routes.js
@@ -1,13 +1,12 @@
 // require importa paquetes externos y funciones creadas por ti.
-const {Router, response} = require('express');
+const { Router, response } = require('express');
 const router = Router();
 
 // con el / te muestra las carpetas de tu sistema, ./ te muestra la carpeta actual, ../ va hacia una carpeta atras, ../../ mas carpetas hacia atras
-const {getUsuario, postUsuario, deleteUsuario, updateUsuario} = require('./usuarios');
+const { getUsuario, postUsuario, deleteUsuario, updateUsuario } = require('./usuarios');
 
 router.get('/user', getUsuario);
 router.post('/user', postUsuario);
 router.delete('/user', deleteUsuario);
-router.put('/user',updateUsuario);
+router.put('/user', updateUsuario);
 module.exports = router;
-//
